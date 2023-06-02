acdf2037d4a05355ceaf14c839a0e4db0c078c2d
Ryan Eastman <rking1323@gmail.com>
2023-06-01 22:41:22 -0700

    added vendor tests

--- clients/vendor/vendor-handler.test.js
+++ clients/vendor/vendor-handler.test.js
@@ -1,35 +1,57 @@
 'use strict';
 
 
-const socket = require('../clients/socket.js');
-// const handler= require('./handler');
-const { packageDelivered, generateOrder } = require('./handler.js');
+const { io } = require('socket.io-client');
+const socket = io('http://localhost:3001/caps');
 
-jest.mock('../socket.js', () => {
+const { orderHandler, thankDriver } = require('./handler');
+const { default: JestHasteMap } = require('jest-haste-map');
+const { test } = require('node:test');
+
+jest.mock('socket.io-client', () => {
   return {
+    io: jest.fn().mockReturnThis(),
     on: jest.fn(),
     emit: jest.fn(),
+    connect: jest.fn(),
   };
 });
-console.log = jest.fn();
 
+let consoleSpy;
 
+beforeEach(() => {
+  consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+}
+);
 
-describe('handle vendor', () => {
-  let payload = {
-    store: 'test store',
-    orderID: 'testOrder',
-    customer: 'testCustomer',
-    address: 'testAddress',
-  }
-  it('creates the order payload', () => {
-    generateOrder(socket, payload);
-    expect(console.log).toHaveBeenCalledWith('VENDOR: Order ready for pickup.');
+afterEach(() => {
+  consoleSpy.mockRestore();
+}
+);
+
+describe('Testing Vendor Handlers', () => {
+  test('Should log correct emit and console.log() for orderHandler', () => {
+    let payload = {
+      orderId: '1234',
+    };
+
+    orderHandler(payload);
+
     expect(socket.emit).toHaveBeenCalledWith('pickup', payload);
+  });
 
+  test('Should log correct emit and console.log() for deliveredMessage', () => {
+    let payload = {
+      customer: 'TEST TEST',
+    };
+
+    thankDriver(payload);
+
+    expect(consoleSpy).toHaveBeenCalledWith(`VENDOR: Thank you for your order ${payload.orderID}`);
   });
-  it('confirms delivery', () => {
-    packageDelivered(payload);
-    expect(console.log).toHaveBeenCalledWith(`VENDOR: Thank you for delivering ${payload.orderID}`);
-  });
+
 });
+
+
+
+
